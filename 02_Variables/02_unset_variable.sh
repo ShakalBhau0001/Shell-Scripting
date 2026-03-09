851c447adc194e7917aae3b62f85dd3ef7f3eea9
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-03-09 14:29:14 +0530

    Update 02_unset_variable.sh

--- 02_Variables/02_unset_variable.sh
+++ 02_Variables/02_unset_variable.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+name="Shakal"
+age=19
+
+echo "Name is $name and age is $age"
+
+# unset name
+# We can unset both variables at once
+unset name age
+
+echo "Name is $name and age is $age"
 
