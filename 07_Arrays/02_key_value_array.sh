953e6e1194707ff9ca4cb51e58eb521e898b68d3
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-03-15 14:38:30 +0530

    Update 02_key_value_array.sh

--- 07_Arrays/02_key_value_array.sh
+++ 07_Arrays/02_key_value_array.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
+# Make the key value pair array
 
+
+# Syntax :
+# declare -A array_name
+# my_array=( [key1]=value1 [key2]=value2 )
+
+# echo ${array_name[key_name]}
+
+# Ex:
+
+declare -A my_array
+my_array=( [name]=Shakalen [age]=19 )
+
+echo "Name is: ${my_array[name]}"     # Name is: Shakalen
+
+#  OR
+
+declare -A my_array
+my_array=( ["name"]="Shakalen" ["age"]=19 )
+
+echo "Name is: "${my_array["name"]}        # Name is: Shakalen
+
+# Both are working
